6341176df09e2fd36cfe6c7f9d2afb31235606e8
naif_alfardan <nayfsaad@gmail.com>
2023-01-11 01:33:52 +0300

    first commit

---
+++ SmartContarct.sol
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier:UNLICENSED
+pragma solidity ^0.8.4;
+
+contract Student {
+    uint256 totalwaves;
+
+    event NewStudent(address indexed student, string name, uint256 id);
+
+    struct Student {
+        address student;
+        string name;
+        uint256 id;
+    }
+
+    Student[] students;
+
+    function setStudentsInfo(string memory _name, uint256 _id) public {
+        students.push(Student(msg.sender, _name, _id));
+        emit  NewStudent(msg.sender, _name, _id);
+    }
+
+    function getStudentInfo() public view returns (Student[] memory){
+        return students;
+    }
+}
